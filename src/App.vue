be099dcf85bcecf9e05b2f65727e0cf9894b8e34
SatyamKandoi <satyam.19beceg003@gmail.com>
2023-08-28 18:39:04 +0530

    First cOMMIT

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Home from "./components/Header.vue";
+import Slider from "./components/Slider.vue";
+import { ref } from "vue";
+
+const example = ref("");
+</script>
+
+<template>
+  <div>
+    <Home></Home>
+    <Slider></Slider>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <transition-group name="fade" tag="div">
+      <div v-for:"i in [currentIndex]" :key="i">
+
+      </div>
+
+    </transition-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const images: string[] = [
+      "https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg",
+    ];
+    const timer = ref<number>();
+    const currentIndex = ref<number>(0);
+
+    const startSlide = () => {
+      timer.value = setInterval(next, 4000);
+    };
+    const next = () => {
+      currentIndex.value++;
+    };
+    const prev = () => {
+      currentIndex.value -= 1;
+    };
+
+    onMounted(() => {
+      startSlide();
+    });
+  },
+});
+</script>
